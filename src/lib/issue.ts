ee4096574276299bb3219f7d8c55f1e4c3dd6ed0
Clay Miller <clay@smockle.com>
2021-08-16 17:49:24 -0400

    chore: publish release branch [skip ci]

--- src/lib/issue.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node --es-module-specifier-resolution=node
-
-import type { RestEndpointMethodTypes } from "@octokit/rest"
-type IssueResponse = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
-
-export function getBody(issueResponse: IssueResponse) {
-  return issueResponse.body
-}
